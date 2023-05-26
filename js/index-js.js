59f0642b5ed595d354c8aec75dae176df3df8070
Bruno Costa <91901625+bfc1557@users.noreply.github.com>
2023-05-26 15:32:45 +0100

    Fix das hiperligações

--- js/index-js.js
+++
@@ -1,7 +0,0 @@
-function enviarMensagem(){
-    const nome = document.getElementById("exampleFormControlInput1");
-    const email = document.getElementById("exampleFormControlInput2");
-    const texto = document.getElementById("exampleFormControlTextarea1");
-
-    
-}
