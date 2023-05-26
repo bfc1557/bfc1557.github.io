3d8a0e0190a553d72000a60e3bac30646e718122
Bruno Costa <91901625+bfc1557@users.noreply.github.com>
2023-05-26 12:26:54 +0100

    1ver da pagina sobre

---
+++ js/index-js.js
@@ -0,0 +1,7 @@
+function enviarMensagem(){
+    const nome = document.getElementById("exampleFormControlInput1");
+    const email = document.getElementById("exampleFormControlInput2");
+    const texto = document.getElementById("exampleFormControlTextarea1");
+
+    
+}
